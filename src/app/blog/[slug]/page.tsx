3db47fef8811f06c1849d3299e38e4855f207fd9
hussam95 <hussam.ulhaq@yahoo.com>
2025-04-29 21:47:10 +0500

    fix: slug page build error

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -7,53 +7,52 @@ import {
   BlogImage
 } from "@/components/styled/Blog";
 
-export async function generateMetadata({
-    params,
-  }: {
-    params: { slug: string };
-  }) {
-    const { frontmatter } = await getPost(params.slug);
-    const { title, description } = frontmatter;
-  
-    return {
+type Params = Promise<{ slug: string }>;
+
+export async function generateMetadata(props: { params: Params }) {
+  const { slug } = await props.params;
+  const { frontmatter } = await getPost(slug);
+  const { title, description } = frontmatter;
+
+  return {
+    title,
+    description,
+    openGraph: {
       title,
       description,
-      openGraph: {
-        title,
-        description,
-        type: "article",
-        url: `https://aasaanfinance.com/blog/${params.slug}`,
-        images: [
-          {
-            url: `https://aasaanfinance.com/${params.slug}.png`,
-            width: 1200,
-            height: 630,
-            alt: title,
-          },
-        ],
-      },
-      twitter: {
-        card: "summary_large_image",
-        title,
-        description,
-        images: [`https://aasaanfinance.com/${params.slug}.png`],
-      },
-    };
-  }
-  
-  export default async function BlogPost({
-    params,
-  }: {
-    params: { slug: string };
-  }) {
-    const { content } = await getPost(params.slug, {
-      BlogContainer,
-      Section,
-      SectionTitle,
-      Paragraph,
-      BlogImage,
-    });
-  
-    return <BlogContainer>{content}</BlogContainer>;
-  }
-  
+      type: "article",
+      url: `https://aasaanfinance.com/blog/${slug}`,
+      images: [
+        {
+          url: `https://aasaanfinance.com/${slug}.png`,
+          width: 1200,
+          height: 630,
+          alt: title,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [`https://aasaanfinance.com/${slug}.png`],
+    },
+  };
+}
+
+export default async function BlogPost(props: { params: Params }) {
+  const { slug } = await props.params;
+  const { content } = await getPost(slug, {
+    BlogContainer,
+    Section,
+    SectionTitle,
+    Paragraph,
+    BlogImage,
+  });
+
+  return (
+    <BlogContainer>
+      {content}
+    </BlogContainer>
+  );
+}
