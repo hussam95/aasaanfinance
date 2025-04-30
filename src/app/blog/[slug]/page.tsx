17756e6dabe00bfd9a2215d056238e4780857b65
hussam95 <hussam.ulhaq@yahoo.com>
2025-04-30 08:08:15 +0500

    fix: OG images not appearing

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -24,7 +24,7 @@ export async function generateMetadata(props: { params: Params }) {
       url: `https://aasaanfinance.com/blog/${slug}`,
       images: [
         {
-          url: `https://aasaanfinance.com/${slug}.png`,
+          url: `https://aasaanfinance.com/blog-imgs/${slug}.png`,
           width: 1200,
           height: 630,
           alt: title,
@@ -35,7 +35,7 @@ export async function generateMetadata(props: { params: Params }) {
       card: "summary_large_image",
       title,
       description,
-      images: [`https://aasaanfinance.com/${slug}.png`],
+      images: [`https://aasaanfinance.com/blog-imgs/${slug}.png`],
     },
   };
 }
