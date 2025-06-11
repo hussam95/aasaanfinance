31ee61d2b3421565d449dc1f913c180364217522
hussam95 <hussam.ulhaq@yahoo.com>
2025-06-11 18:36:31 +0500

    Add sitemap and robots.txt using next-sitemaptemp

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.aasaanfinance.com',
+  generateRobotsTxt: true,              
+  generateIndexSitemap: true,      
+};
